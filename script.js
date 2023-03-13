43b3ccc33c975e35f9687449775dae525cac542e
Sambit_Sahoo <93509732+Sahoo-Sambit@users.noreply.github.com>
2023-03-14 01:25:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Your Script here.
+function ROT13(str) {
+	var str = "";
+	for (var i = 0; i < asciiArr.length; i++) {
+		var ch=asciiArr.charCodeAt(i);
+	  if(ch>=65 && ch<=90){
+	    str += String.fromCharCode((ch-65+13)%26+65);
+	  }
+	}
+	return str;
+}
