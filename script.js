bde3329c567efd0fd40038f5728097bbbc84f85a
Sambit_Sahoo <93509732+Sahoo-Sambit@users.noreply.github.com>
2023-03-15 13:28:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-function rot13(str) {
+const output=document.getElementById("output")
+function rot13() {
+  const str= document.getElementById("input").value;
   var res = "";
 	for (var i = 0; i < str.length; i++) {
     var ch=str.charCodeAt(i);
+    console.log(ch);
     if(ch>=65 && ch<=90){
 	    res += String.fromCharCode((ch-65+13)%26+65);
 	  }
     else res+=str.charAt(i);
 	}
-  return res;
+  output.innerText = res;
 }
